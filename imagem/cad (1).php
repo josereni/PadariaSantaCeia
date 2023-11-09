ff1460988554c2ec87725bf94c0f3f7a32859408
Victor Leandro <vleandro2006@hotmail.com>
2023-11-08 21:45:39 -0300

    padaria santa ceia
    fiz o meu código até a sessão dos produtos

---
+++ imagem/cad (1).php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="estilo.css">
+    
+</head>
+<body>
+     <header>
+        <h1>Cadastro de clientes | Emporio Padaria</h1>
+     </header>
+
+     <main class="text">
+        <?php 
+        $nome = $_GET["nome"];
+        $Sobrenome = $_GET["sobrenome"];
+        $tel = $_GET["tel"];
+
+        echo "<p>É um prazer te conhecer, a Padaria Victor Agradece $nome
+        $Sobrenome</p>"
+        ?>
+
+        <button class="text-js">
+         <a href="javascript:history.go(-1)">Voltar para página anterior</a>
+      
+      </button>
+     </main>
+     
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+</html>
